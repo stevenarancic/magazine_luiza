fc2f75d0433c94d00560d66f7066856f1bc9e861
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2021-08-12 23:25:03 -0300

    Update price_tag.dart

--- lib/screens/home/widgets/price_tag.dart
+++ lib/screens/home/widgets/price_tag.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
 class PriceTag extends StatelessWidget {
+  final double priceTag;
+  final int numberOfInstallments;
+
+  PriceTag({
+    required this.priceTag,
+    required this.numberOfInstallments,
+  });
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Text(
+      "${this.numberOfInstallments}x de METODO CALCULAR PARCELA sem juros",
+    );
   }
 }
